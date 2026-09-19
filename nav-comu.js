a612968e6e2781be68d484cabb19df3dca0409a6
genecarles <genecarles@gmail.com>
2026-09-19 09:43:45 +0200

    Fix commented out div in NavComu component

--- nav-comu.js
+++ nav-comu.js
@@ -9,9 +9,9 @@ class NavComu extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <nav class="navbar">
-                <! div class="navbar-logo">
+                <!-- div class="navbar-logo">
                     <img src="${CONFIG.ASSETS}${CONFIG.LOGO}" alt="${CONFIG.NOM}">
-                </div>
+                </div-->
                 <button class="navbar-hamburguesa">☰</button>
                 <ul class="navbar-menu">
                     <li><a href="#Inici">${CONFIG.NAV_INICI}</a></li>
